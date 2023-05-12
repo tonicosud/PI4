2b7b21a8cd7b864bb7ccdfcbfcc656ea43c67dcb
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-05-12 13:03:14 -0300

    Update PINTEGRADOR.py

--- PINTEGRADOR.py
+++ PINTEGRADOR.py
@@ -40,7 +40,7 @@ df['month'] = date_col.month
 df['year'] = date_col.year
 df['data'] = df['month'].map(str) + '/' + df['year'].map(str)
 
-fig = px.bar(df, x="data", y=column, title=column + ' - ' + state,color="Número acumulado de casos", color_continuous_scale=px.colors.sequential.Inferno)#color_continuous_scale = 'Inferno')
+fig = px.bar(df, x="date", y=column, title=column + ' - ' + state,color="Número acumulado de casos", color_continuous_scale = 'Inferno')
 fig.update_layout( 
     updatemenus=[
             dict(
