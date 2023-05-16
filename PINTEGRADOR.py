7a8311b4d6bc1af116d75a9c407b22895f84b546
Antonio Santos <86670111+tonicosud@users.noreply.github.com>
2023-05-16 08:28:41 -0300

    Update PINTEGRADOR.py

--- PINTEGRADOR.py
+++ PINTEGRADOR.py
@@ -10,55 +10,25 @@ Original file is located at
 import pandas as pd
 import plotly.express as px
 import streamlit as st 
-from pandas import DataFrame
 
 #streamlit run codigoBase.py
 
-
 df = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv')
 
-
 df = df.rename(columns={'totalCases': 'Número acumulado de casos','newCases': 'Novos casos','deaths': 'Número acumulado de óbitos','newDeaths': 'Novos óbitos','vaccinated': 'Vacinas aplicadas - primeira dose','vaccinated_second': 'Vacinas aplicadas - segunda dose','vaccinated_single': 'Vacinas aplicadas - dose única'})
-
 
 state  = 'SP'
 estados = list(df['state'].unique())
 #estados = list(df['SP'].unique())
 #state = st.sidebar.selectbox('ESTADO', SP)
 
-
-
 colunas = ['Número acumulado de casos','Novos casos','Número acumulado de óbitos','Novos óbitos','Vacinas aplicadas - primeira dose','Vacinas aplicadas - segunda dose','Vacinas aplicadas - dose única']
 column = st.sidebar.selectbox('Qual tipo de informação?', colunas)
 
-
 df = df[df['state'] == state]
 
-date_col = pd.DatetimeIndex(df['date'])
-
-df['month'] = date_col.month
-df['year'] = date_col.year
-df['data'] = df['month'].map(str) + '/' + df['year'].map(str)
-
 fig = px.bar(df, x="date", y=column, title=column + ' - ' + state, color = column, color_continuous_scale = 'Bluered')
-fig.update_layout( 
-    updatemenus=[
-            dict(
-                buttons=list([
-                    dict(
-                        args=["type", "line"],
-                        label="Line",
-                        method="restyle"
-                    ),
-                    dict(
-                        args=["type", "bar"],
-                        label="Bar Chart",
-                        method="restyle"
-                    )
-                ]),
-                direction="down",
-            ),        
-        ])
+
 
 st.title('DADOS COVID-19')
 st.write('Nessa aplicação, o usuário tem a possibilidade de interação,e visualização de dados sobre  covid-19. Utilize o menu lateral para alterar a mostragem.')
@@ -66,5 +36,3 @@ st.write('Nessa aplicação, o usuário tem a possibilidade de interação,e vis
 st.plotly_chart(fig, use_container_width=True)
 
 st.caption('Projeto desenvolvido por alunos UNIVESP,PROJETO INTEGRADOR IV')
-
-#fig.show()
